18058b4a2454f04d4bcf6db956b4757de9b1c5fd
Alejandro <alejandrofpo@gmail.com>
2018-09-09 23:19:02 +0200

    ADD NGXS AND REDUX DEV TOOLS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from '@layout/layout.module';
@@ -10,6 +13,8 @@ import { LayoutModule } from '@layout/layout.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    NgxsModule.forRoot([]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
     AppRoutingModule,
     LayoutModule
   ],
